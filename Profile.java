5166185b0c16539390b7d3eb5c6d036cac0e9c71
Raihanki <Raihanhori>
2020-09-16 09:57:49 +0700

    Membuat profile nama,kelas,nim dengan java

---
+++ Profile.java
@@ -0,0 +1,14 @@
+public class Profile{
+	public static void main(String args[])
+	{
+		System.out.println("Nama: Raihan Khoerul Imam");
+		System.out.println("Nim: 6706202033 ");
+		System.out.println("Kelas: D3IF-44-02");
+	}
+}
+
+
+
+
+
+
